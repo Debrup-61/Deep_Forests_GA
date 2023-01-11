e79867828b4cd58ee1694ee1fa901b78e2c8bfce
Debrup Das <70255493+Debrup-61@users.noreply.github.com>
2023-01-11 16:38:04 +0530

    Create cube.m

---
+++ Math_functions/cube.m
@@ -0,0 +1,9 @@
+function r = cube(a)
+%CUBE Summary of this function goes here
+%   Detailed explanation goes here
+r = power(a,3);
+%if r == zeros(size(a))
+    %r = inf*ones(size(a));
+%end
+
+end
